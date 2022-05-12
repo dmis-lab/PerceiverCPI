9ed28df388c123f0bb3c48cb0467d67528acc7be
James <32150689+NathanielNguyen11@users.noreply.github.com>
2022-05-12 14:17:46 +0900

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+echo "Please enter the GPU address"
+read GPU
+echo "Sellected GPU: $GPU"
+
+CUDA_VISIBLE_DEVICES=$GPU nohup python train.py --data_path '' --separate_val_path '' --separate_test_path '' --metric mse --dataset_type regression --save_dir '' --target_columns label --epochs 150 --ensemble_size 2 --num_folds 1 --batch_size 50 --aggregation mean --dropout 0.1 --save_preds >& out_name.out &
